3b8fa99f971f5755fb0cf561a9a207bcb99e850c
Philip Källberg <philip@cloud66.com>
2014-04-11 16:31:25 +0200

    Update append.sh

--- .cloud66/append.sh
+++ .cloud66/append.sh
@@ -1,1 +1,3 @@
+cd /usr/share/elasticsearch/bin && plugin -u https://s3.amazonaws.com/www.emergi.net/elasticsearch/plugins/elasticsearch-jetty-1.0.0.zip -i elasticsearch-jetty-1.0.0
 echo "http.type: com.sonian.elasticsearch.http.jetty.JettyHttpServerTransportModule" >> /etc/elasticsearch/elasticsearch.yml
+sudo service elasticsearch restart
